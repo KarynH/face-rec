46d2090fffaadfd951a6c107202f065aed39262d
Karyn Huston <huston.karyn@gmail.com>
2024-11-02 14:50:34 -0400

    bug in get_face

--- main.py
+++ main.py
@@ -29,15 +29,26 @@ async def get_all_faces():
 
 '''
 
-@app.get("/get-face/")
+@app.get("/get-face")
 async def get_all_faces():
-    faces = list(collection.find())
-    all_faces = [{"student_name": face["student_name"], "face_encoding": face["face_encoding"]} for face in faces]
-   
-    if all_faces:
-        return all_faces
-    else:
-        raise HTTPException(status_code=404, detail="No face data found.")
+    try:
+        faces = list(collection.find({}, {"_id": 0}))  # Exclude _id for JSON serialization
+        all_faces = [
+            {
+                "student_name": face.get("student_name", "Unknown"), 
+                "face_encoding": face.get("face_encoding", [])
+            }
+            for face in faces
+        ]
+        
+        if all_faces:
+            return all_faces
+        else:
+            raise HTTPException(status_code=404, detail="No face data found.")
+    
+    except PyMongoError as e:
+        print(f"Error fetching face data: {e}")
+        raise HTTPException(status_code=500, detail="Error fetching face data from the database.")
 
 '''
 run server locally:
