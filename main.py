3d44f978525d60a06e68ba50beed0ee05706e1d9
Karyn Huston <huston.karyn@gmail.com>
2024-11-02 14:22:08 -0400

    get_face request

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 class FaceEncoding(BaseModel):
     student_name: str
-    student_id: str
+    student_id: int
     face_encoding: List[float]
     
 @app.post("/store-face")
@@ -16,13 +16,27 @@ async def store_face_data(data: FaceEncoding):
     collection.insert_one(data.dict())
     return {"message": "Face encoding stored successfully."}
 
-@app.get("/get-face/{student_id}")
-async def get_face_data(student_id: str):
+'''
+async def get_all_faces():
+    faces = collection.find()
+    all_faces = [{"student_name": face["student_name"], "face_encoding": face["face_encoding"]} for face in faces]
+   
+    if all_faces:
+        return all_faces
+    else:
+        raise HTTPException(status_code=404, detail="No face data found.")
+
+
+'''
+
+@app.get("/get-face/")
+async def get_face_data():
     # Fetch the encoding by student ID
-    face_data = collection.find_one({"student_id": student_id})
+    face_data = collection.find([{"student_name": face["student_name"], "face_encoding": face["face_encoding"]} for face in faces])
     if face_data:
-        return {"student_id": face_data["student_id"], "face_encoding": face_data["face_encoding"]}
-    raise HTTPException(status_code=404, detail="Face data not found")
+        return face_data
+    else:
+        raise HTTPException(status_code=404, detail="No face data found.")
 
 '''
 run server locally:
